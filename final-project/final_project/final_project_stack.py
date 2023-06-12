131fedd749ddb8f3875af121a910a705ffa51ae7
CurtPolo <129736085+CurtPolo@users.noreply.github.com>
2023-06-12 15:17:20 +0200

    update
    code still not fully working

--- final-project/final_project/final_project_stack.py
+++ final-project/final_project/final_project_stack.py
@@ -1,19 +1,81 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    core,
 )
-from constructs import Construct
 
-class FinalProjectStack(Stack):
+class FinalProjectStack(core.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        web_vpc = ec2.Vpc(
+            self,
+            "WebVPC",
+            cidr="10.10.10.0/24",
+            max_azs=2,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="PublicSubnetAZ-A",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                    reserved=False,
+                    availability_zone="eu-central-1a"
+                ),
+                ec2.SubnetConfiguration(
+                    name="PublicSubnetAZ-B",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                    reserved=False,
+                    availability_zone="eu-central-1b"
+                ),
+            ]
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "FinalProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        management_vpc = ec2.Vpc(
+            self,
+            "ManagementVPC",
+            cidr="10.20.20.0/24",
+            max_azs=2,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="PublicSubnetAZ-A",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                    reserved=False,
+                    availability_zone="eu-central-1a"
+                ),
+                ec2.SubnetConfiguration(
+                    name="PrivateSubnetAZ-A",
+                    subnet_type=ec2.SubnetType.PRIVATE,
+                    cidr_mask=24,
+                    reserved=False,
+                    availability_zone="eu-central-1a"
+                ),
+                ec2.SubnetConfiguration(
+                    name="PublicSubnetAZ-B",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                    reserved=False,
+                    availability_zone="eu-central-1b"
+                ),
+            ]
+        )
+
+        transit_gateway = ec2.CfnTransitGateway(
+            self,
+            "TransitGateway",
+            description="CDK Example Transit Gateway"
+        )
+
+        for vpc in [web_vpc, management_vpc]:
+            ec2.CfnTransitGatewayAttachment(
+                self,
+                f"{vpc.node.id}Attachment",
+                transit_gateway_id=transit_gateway.ref,
+                vpc_id=vpc.vpc_id,
+                subnet_ids=vpc.select_subnets().subnet_ids,
+            )
+
+app = core.App()
+FinalProjectStack(app, "FinalProjectStack")
+app.synth()
