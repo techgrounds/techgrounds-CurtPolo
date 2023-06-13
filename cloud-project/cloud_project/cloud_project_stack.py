dc36b4c4736fac10f1709daa3c2022d3d0b3e9dc
CurtPolo <129736085+CurtPolo@users.noreply.github.com>
2023-06-13 10:36:01 +0200

    Update cloud_project_stack.py
    first successful running code to create VPCs, AZs, Transit Gateway and the subnets.

--- cloud-project/cloud_project/cloud_project_stack.py
+++ cloud-project/cloud_project/cloud_project_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2
 )
 from constructs import Construct
 
@@ -10,10 +9,99 @@ class CloudProjectStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # VPC for Web Server
+        web_server_vpc = ec2.Vpc(
+            self, 
+            "WebServerVPC",
+            cidr="10.10.10.0/24",
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="PublicSubnet1", 
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=25
+                ),
+                ec2.SubnetConfiguration(
+                    name="PublicSubnet2", 
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=25
+                )
+            ],
+            availability_zones=['eu-central-1a', 'eu-central-1b']
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CloudProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # VPC for Management Server
+        management_server_vpc = ec2.Vpc(
+            self, 
+            "ManagementServerVPC",
+            cidr="10.20.20.0/24",
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="PublicSubnet1", 
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=25
+                ),
+                ec2.SubnetConfiguration(
+                    name="PublicSubnet2", 
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=25
+                )
+            ],
+            availability_zones=['eu-central-1a', 'eu-central-1b']
+        )
+
+        # Transit Gateway
+        transit_gateway = ec2.CfnTransitGateway(
+            self,
+            "TransitGateway",
+            default_route_table_association="true",
+            default_route_table_propagation="true",
+            description="My Transit Gateway"
+        )
+
+        # Attach VPCs to Transit Gateway
+        web_server_attachment = ec2.CfnTransitGatewayAttachment(
+            self,
+            "WebServerVPCAttachment",
+            transit_gateway_id=transit_gateway.ref,
+            vpc_id=web_server_vpc.vpc_id,
+            subnet_ids=[web_server_vpc.select_subnets(subnet_name="PublicSubnet1", one_per_az=True).subnet_ids[0],
+                        web_server_vpc.select_subnets(subnet_name="PublicSubnet2", one_per_az=True).subnet_ids[0]]
+        )
+
+        management_server_attachment = ec2.CfnTransitGatewayAttachment(
+            self,
+            "ManagementServerVPCAttachment",
+            transit_gateway_id=transit_gateway.ref,
+            vpc_id=management_server_vpc.vpc_id,
+            subnet_ids=[management_server_vpc.select_subnets(subnet_name="PublicSubnet1", one_per_az=True).subnet_ids[0],
+                        management_server_vpc.select_subnets(subnet_name="PublicSubnet2", one_per_az=True).subnet_ids[0]]
+        )
+
+        # Associate each attachment with the Transit Gateway's default route table
+        ec2.CfnTransitGatewayRouteTablePropagation(
+            self,
+            "WebServerVPCTGAttachmentPropagation",
+            transit_gateway_route_table_id=transit_gateway.attr_default_route_table_id,
+            transit_gateway_attachment_id=web_server_attachment.ref
+        )
+
+        ec2.CfnTransitGatewayRouteTableAssociation(
+            self,
+            "WebServerVPCTGAttachmentAssociation",
+            transit_gateway_route_table_id=transit_gateway.attr_default_route_table_id,
+            transit_gateway_attachment_id=web_server_attachment.ref
+        )
+
+        ec2.CfnTransitGatewayRouteTablePropagation(
+            self,
+            "ManagementServerVPCTGAttachmentPropagation",
+            transit_gateway_route_table_id=transit_gateway.attr_default_route_table_id,
+            transit_gateway_attachment_id=management_server_attachment.ref
+        )
+
+        ec2.CfnTransitGatewayRouteTableAssociation(
+            self,
+            "ManagementServerVPCTGAttachmentAssociation",
+            transit_gateway_route_table_id=transit_gateway.attr_default_route_table_id,
+            transit_gateway_attachment_id=management_server_attachment.ref
+        )
