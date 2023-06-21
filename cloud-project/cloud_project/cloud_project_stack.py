5a1656e43b97f6c12d45fcc31bf7c6091cbe4141
CurtPolo <129736085+CurtPolo@users.noreply.github.com>
2023-06-21 16:40:33 +0200

    Update cloud_project_stack.py

--- cloud-project/cloud_project/cloud_project_stack.py
+++ cloud-project/cloud_project/cloud_project_stack.py
@@ -31,6 +31,7 @@ class CloudProjectStack(Stack):
 
         # Create the web server VPC
         vpc = ec2.Vpc(self, "Cloud10VPC",
+            ip_addresses=ec2.IpAddresses.cidr("10.10.10.0/24"),
             max_azs=2,
             subnet_configuration=[
                 ec2.SubnetConfiguration(name="public", cidr_mask=26, subnet_type=ec2.SubnetType.PUBLIC),
